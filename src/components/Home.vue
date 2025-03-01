fc536d68b474f487552e8023dec91b009efd1aea
TheDarkGlobe <lambstream@gmail.com>
2025-03-01 20:52:31 +0100

    Fix custom subtype

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -547,7 +547,7 @@ const tinyMCEConfig = {
                 <div class="mt-2">
                   <div
                       class="flex items-center rounded-md bg-white dark:bg-dark pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-primary">
-                    <input id="cardWeaponSubtypeCustom" v-model="fields.cardWeaponSubtype"
+                    <input id="cardWeaponSubtypeCustom" v-model="fields.cardWeaponSubtypeCustom"
                            class="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 dark:text-white placeholder:text-gray-400 focus:outline-none sm:text-sm/6 "
                            placeholder="Enter custom weapon subtype"
                            type="text">
