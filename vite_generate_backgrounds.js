7d4413a58c49bf0ab24d35586029d3a3c44be2e4
TheDarkGlobe <lambstream@gmail.com>
2025-08-22 17:07:33 +0200

    Add 1 example cardback for each type (#7)

--- vite_generate_backgrounds.js
+++ vite_generate_backgrounds.js
@@ -1,12 +1,40 @@
-import {createWriteStream, promises} from 'fs';
+import {createWriteStream, promises, readFile, copyFile} from 'fs';
 import http from 'http';
 import https from 'https';
+import examples from "./example.json" with {type: "json"};
+
+const getCardbacks = async function (url) {
+    let cardbacks = [];
+    if (!url.startsWith('http') && !url.startsWith('https')) {
+        cardbacks = await getExamples();
+    } else {
+        cardbacks = await getBackendCardbacks(url);
+    }
+
+    return `const cardbacks = ${JSON.stringify(cardbacks)};
+export function useCardBacks() {
+    return cardbacks;
+}`;
+}
 
 
-const getCardbacks = async function(url) {
-    const response = await fetch(url);
-    const cardbacks = await response.json();
-    let downloads = [];
+async function getExamples() {
+    var cardbacks = examples;
+    const downloads = getDownloads(cardbacks);
+
+    downloads.forEach(download => {
+        copyFile(download.url, download.fileLocation, (err) => {
+            if (err) {
+                console.log("Error Copying File:", err);
+            }
+        });
+    })
+
+    return cardbacks;
+}
+
+function getDownloads(cardbacks) {
+    const downloads = [];
     cardbacks.forEach((cardback) => {
         cardback.images.forEach((cardbackImage) => {
             const imgUrl = cardbackImage.fileName.split('/').pop();
@@ -18,6 +46,14 @@ const getCardbacks = async function(url) {
         })
     })
 
+    return downloads;
+}
+
+async function getBackendCardbacks(url) {
+    const response = await fetch(url);
+    const cardbacks = await response.json();
+    const downloads = getDownloads(cardbacks);
+
     const downloadPromises = [];
     for (const download of downloads) {
         await promises.access(download.fileLocation)
@@ -27,10 +63,7 @@ const getCardbacks = async function(url) {
 
     await Promise.all(downloadPromises);
 
-    return `const cardbacks = ${JSON.stringify(cardbacks)};
-export function useCardBacks() {
     return cardbacks;
-}`;
 }
 
 async function downloadFileFromURL(url, fileLocation) {
