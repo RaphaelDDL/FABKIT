2df4ebf72f51f70ddbc31b7ef1e1a8b2ff19a1d3
TheDarkGlobe <lambstream@gmail.com>
2025-09-09 17:48:54 +0200

    Set target="_blank" for generated download links.

--- src/helpers/card.js
+++ src/helpers/card.js
@@ -621,6 +621,7 @@ export function useCard() {
         const link = document.createElement('a');
         link.download = name;
         link.href = uri;
+        link.target = '_blank';
         document.body.appendChild(link);
         link.click();
         document.body.removeChild(link);
